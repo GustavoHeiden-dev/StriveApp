2a8bf40fdbc012395b3a73e6f6e8dbeb49702767
Vinicius Rafael Batista <vinirafaelbatista@gmail.com>
2025-09-29 11:54:59 -0300

    versao8

--- Strive/src/main/java/Servelet/FinalizarTreinoServlet.java
+++ Strive/src/main/java/Servelet/FinalizarTreinoServlet.java
@@ -9,17 +9,26 @@ import Dao.TreinoSessaoDAO;
 @WebServlet("/FinalizarTreinoServlet")
 public class FinalizarTreinoServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
             
         try {
             int idSessao = Integer.parseInt(request.getParameter("idSessao"));
             TreinoSessaoDAO dao = new TreinoSessaoDAO();
             dao.finalizarSessao(idSessao);
             
+            HttpSession session = request.getSession();
+            session.setAttribute("treinoFinalizado", "true");
+            
             response.sendRedirect("TreinoServlet");
 
-        } catch (NumberFormatException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
             response.sendRedirect("TreinoServlet");
         }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
     }
 }
